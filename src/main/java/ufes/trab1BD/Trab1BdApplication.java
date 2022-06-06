5b04d3f7f790ff67dd3eebf06e991bd9c5081bcc
enzocussuol <enzo@allsystem.com.br>
2022-06-06 20:07:38 -0300

    Estrutura do projeto (maven)

---
+++ src/main/java/ufes/trab1BD/Trab1BdApplication.java
@@ -0,0 +1,13 @@
+package ufes.trab1BD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trab1BdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trab1BdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ufes/trab1BD/Trab1BdApplicationTests.java
@@ -0,0 +1,13 @@
+package ufes.trab1BD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trab1BdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
